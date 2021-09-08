f119c215a56a66f33a837da2ab273ecea0fc433d
Mikolaj Rdzanek <mikolaj.rdzanek@sick.de>
2021-09-08 13:04:25 +0200

    'Update'

--- KneeDetection/scripts/kneeDetection.lua
+++ KneeDetection/scripts/kneeDetection.lua
@@ -1,28 +1,3 @@
---[[----------------------------------------------------------------------------
-
-  Application Name:
-  KneeDetection
-
-  Summary:
-  Detects knees in a profile
-
-  Description:
-  Explains how to use the second derivative of a profile to find knees
-
-  How to run:
-  Starting this sample is possible either by running the app (F5) or
-  debugging (F7+F10). Setting breakpoint on the first row inside the 'main'
-  function allows debugging step-by-step after 'Engine.OnStarted' event.
-  Results can be seen in the image viewer on the DevicePage.
-  Restarting the Sample may be necessary to show the profiles after loading the webpage.
-  To run this Sample a device with SICK Algorithm API and AppEngine >= V2.5.0 is
-  required. For example SIM4000 with latest firmware. Alternatively the Emulator
-  on AppStudio 2.3 or higher can be used.
-
-  More Information:
-  Tutorial "Algorithms - Profile - FirstSteps".
-
-------------------------------------------------------------------------------]]
 
 --Start of Global Scope---------------------------------------------------------
 
